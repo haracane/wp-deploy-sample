441a852c3ceb52e636f72d8c07d1b2f83a1e4f72
haracane <haracane@gmail.com>
2014-11-13 18:27:16 +0900

    Update config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,9 @@
 ############################################
 
 set :stage, :production
-set :stage_url, "http://www.example.com"
-server "XXX.XXX.XX.XXX", user: "SSHUSER", roles: %w{web app db}
-set :deploy_to, "/deploy/to/path"
+set :stage_url, "http://localhost"
+server "localhost", user: ENV['USER'], roles: %w{web app db}
+set :deploy_to, "/Users/haracane/public_html/wp-deploy-sample.production"
 
 ############################################
 # Setup Git
